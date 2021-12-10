989410ad50a261f39661c55083aea595bfe6e061
Jennie <46772772+Jennie-ark@users.noreply.github.com>
2021-12-10 22:49:18 +0700

    Update production.py

--- travel/settings/production.py
+++ travel/settings/production.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = str(os.environ.get('DEBUG'))
 
-ALLOWED_HOSTS = ['https://routessearch.herokuapp.com']
+ALLOWED_HOSTS = ['routessearch.herokuapp.com']
 
 # Application definition
 
